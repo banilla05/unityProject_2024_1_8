68261972bf67e131329ad8ad092dd15bb6af8bc6
banilla05 <yeon.lavin05@gmail.com>
2024-05-21 17:28:54 +0900

    [feat] 게임 점수 및 베스트점수까지

--- UnityProject_1_B/Assets/Scripts/MainGame/CircleObject.cs
+++ UnityProject_1_B/Assets/Scripts/MainGame/CircleObject.cs
@@ -8,11 +8,24 @@ public class CircleObject : MonoBehaviour
     public bool isUsed;                     //사용 완료 판단하는 (bool)
     Rigidbody2D rigidbody2D;                //2D 강체를 불러온다.
 
-    // Start is called before the first frame update
+    public int index;                       //과일 번호를 만든다.
+
+    public float EndTime = 0.0f;            //종료 선 시간 체크 변수(float)
+    public SpriteRenderer spriteRenderer;   //종료시 스프라이트 색을 변환 시키기 위해 접근 선언
+
+    public GameManager gameManager;         //GameManager 접근 선언
+
+    void Awake()
+    {
+        isUsed = false;                                      //사용 완료가 되지 않으(처음 사용)
+        rigidbody2D = GetComponent<Rigidbody2D>();           //강체를 가져온다.
+        rigidbody2D.simulated = false;                       //생성될때는 시뮬레이팅이 되지 않는다.
+        spriteRenderer = GetComponent<SpriteRenderer>();     //해당 오브젝트의 스프라이트 랜더러 접근
+    }
+
     void Start()
     {
-        isUsed = false;                                 //사용 완료가 되지 않으(처음 사용)
-        rigidbody2D = GetComponent<Rigidbody2D>();      //강체를 가져온다.
+        gameManager = GameObject.FindWithTag("GameManager").GetComponent<GameManager>();        //게임 매니저를 얻어온다.
     }
 
     // Update is called once per frame
@@ -54,6 +67,66 @@ public class CircleObject : MonoBehaviour
         if(Temp != null )                                               //해당 오브젝트가 존재하면
         {
             Temp.gameObject.GetComponent<GameManager>().GenObject();    //GameObject 함수를 호출 한다. (GetComponent 통해서 접근한다.
+        }
+    }
+
+    public void Used()
+    {
+        isDrag = false;                     //드래그가 종료
+        isUsed = true;                      //사용이 완료
+        rigidbody2D.simulated = true;              //물리 현상 시작
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)       //Trigger 충돌 중일 때
+    {
+        if(collision.tag == "EndLine")                      //충돌중인 물체가의 TAG 가 EndLine 일 경우
+        {
+            EndTime += Time.deltaTime;                      //프레임시작만큼 누적 시켜서 초를 만든다.
+
+            if(EndTime > 1)                                 //충돌진행이 1초 되었을 경우
+            {
+                spriteRenderer.color = new Color(0.9f, 0.2f, 0.2f);     //빨강색 처리
+            }
+            if(EndTime > 3)
+            {
+                gameManager.EndGame();
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "EndLine")                     //충돌 물체가 빠져 나갔을 때
+        {
+            EndTime = 0.0f;
+            spriteRenderer.color = Color.white; ;           //기존 색상으로 변경
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)       //2D 충돌이 일어날 경우
+    {
+        if (index >= 7)                     //준비된 과일이 최대 7개
+            return;
+
+        if (collision.gameObject.tag == "Fruit")                //충돌 물체의 TAG 가 Fruit 일 경우
+        {
+            CircleObject temp = collision.gameObject.GetComponent<CircleObject>();      //임시로 Class temp를 선언하고 충돌체의 Class(CircleObject)를 받아온다.
+
+            if(temp. index == index)                //과일 번호가 같은 경우
+            {
+                if(gameObject.GetInstanceID() > collision.gameObject.GetInstanceID())   //유니티에서 지원하는 고유의 ID를 받아와서 ID가 큰쪽에서 다음 과일 생성
+                {
+                    //GameManager 에서 생성함수 호출
+                    GameObject Temp = GameObject.FindWithTag("GameManager");           //Tag : GameManager를  Scene 찾아서 오브젝트를 가져온다.
+                    if (Temp != null)
+                    {
+                        Temp.gameObject.GetComponent<GameManager>().MergeObject(index, gameObject.transform.position);   //생성된 MergeObject 함수에 인수와 함께 전달
+                    }
+
+                    Destroy(temp.gameObject);           //충돌 물체 파괴
+                    Destroy(gameObject);                //자기 자신 파괴
+                }
+            }
         }
     }
 }
--- UnityProject_1_B/Assets/Scripts/MainGame/GameManager.cs
+++ UnityProject_1_B/Assets/Scripts/MainGame/GameManager.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject CircleObject;             //과일 프리팹 오브젝트
-    public Transform GenTransform;              //과일이 생성될 위치 오브젝트
-    public float TimeCheck;                     //시간을 체크하기 위한 (float) 값
-    public bool isGen;                          //생성 완료 체크 (bool) 값
+    public GameObject[] CircleObject;                          //과일 프리팹 오브젝트
+    public Transform GenTransform;                             //과일이 생성될 위치 오브젝트
+    public float TimeCheck;                                    //시간을 체크하기 위한 (float) 값
+    public bool isGen;                                         //생성 완료 체크 (bool) 값
 
-    // Start is called before the first frame update
+    public int Point;                                         //점수 값 선언(int)
+    public int BestScore;                                     //스코어 값 선언 (int)
+    public static event Action<int> OnPointChanged;           //event Action 선언 (Point 값이 변경될 경우 호출)
+    public static event Action<int> OnBestScoreChanged;       //event Action 선언 (Point 값이 변경될 경우 호출)
+
     void Start()
     {
-        GenObject();                        //게임이 시작되었을때 함수를 호출해서 초기화 시킨다.
+        BestScore = PlayerPrefs.GetInt("BestScore");           //기존에 저장된 점수를 불러온다.
+        GenObject();                                                    //게임이 시작되었을때 함수를 호출해서 초기화 시킨다.
+        OnPointChanged?.Invoke(Point);
+        OnBestScoreChanged?.Invoke(BestScore);
     }
 
     // Update is called once per frame
@@ -23,7 +31,8 @@ public class GameManager : MonoBehaviour
             TimeCheck -= Time.deltaTime;            //매 프레임마다 프레임 시간을 빼준다.
             if(TimeCheck <= 0 )                     //해당 초 시간이 지났을 경우 (1초 -> 0초가 되엇을 경우_
             {
-                GameObject Temp = Instantiate(CircleObject);    //과일프리팹 오브젝트를 생성 시켜준다. (Instantiate)
+                int RandNumber = UnityEngine.Random.Range(0, 3);            // 0 ~ 2 까지의 랜덤 숫자를 생성
+                GameObject Temp = Instantiate(CircleObject[RandNumber]);    //과일프리팹 오브젝트를 생성 시켜준다. (Instantiate)
                 Temp.transform.position = GenTransform.position;    //설정한 위치로 이동시킨다.
                 isGen = true;                                       //Gen이 되었다고 true로 bool 값을 변경한다.
             }
@@ -34,5 +43,25 @@ public class GameManager : MonoBehaviour
     {
         isGen = false;      //초기화 : isGen을 false (생성 되지 않았다)
         TimeCheck = 1.0f;   //1초후 과일 프리팹을 생성 시키기 위한 초기화
+    }
+
+    public void MergeObject (int index, Vector3 position)       //Merge 함수는 과일번로(int) 과 생성 위치값(vector3)을 전달 받는다.
+    {
+        GameObject Temp = Instantiate(CircleObject[index]);     //index를 그대로 쓴다. (0 부터 배열이 시작되지만 index 값이 1더 있어서)
+        Temp.transform.position = position;                     //위치는 전달 받은 값으로 사용
+        Temp.GetComponent<CircleObject>().Used();               //선언한 Used 함수 사용
+
+        Point += (int)Mathf.Pow(index, 2) * 10;                 //index의 2승으로 점수 포인트 증가 Pow 함수 사용
+        OnPointChanged?.Invoke(Point);                          //포인트가 변경되었을때 이벤트에 변경 되었다고 알림
+    }
+
+    public void EndGame()
+    {
+        if(Point > BestScore)                                   //포인트와 비교한다
+        {
+            BestScore = Point;
+            PlayerPrefs.SetInt("BestScore", Point);             //포인트가 더 클경우 저장한다.
+            OnBestScoreChanged?.Invoke(BestScore);
+        }
     }
 }
