cf65076482d9b22ba8c17902fc8a686eeee3d539
banilla05 <yeon.lavin05@gmail.com>
2024-05-29 14:22:47 +0900

    [feat] 사운드 추가까지

---
+++ UnityProject_1_B/Assets/Scripts/MainGame/ExSoundPlay.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExSoundPlay : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))                //1번을 누르면
+        {
+            SoundManager.instance.PlaySound("BackGround");  //BackGround 재생
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))                //2번을 누르면
+        {
+            SoundManager.instance.PlaySound("Cannon");      //Cannon 재생
+        }
+    }
+}
